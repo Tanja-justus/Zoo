295d25e8fa8900e2a6bc147450d59baafede9aa4
tanjajustus <tbakhoment@gmail.com>
2025-02-10 02:48:56 -0800

    Project Zoo: record

---
+++ src/main/java/org/example/Tier.java
@@ -0,0 +1,4 @@
+package org.example;
+
+public record Tier(int id, String name, String art, int alter, Besitzer besitzer, TierArt futterInGramm) {
+}
---
+++ src/main/java/org/example/TierArt.java
@@ -0,0 +1,4 @@
+package org.example;
+
+public record TierArt(String name, int futterInGramm) {
+}
